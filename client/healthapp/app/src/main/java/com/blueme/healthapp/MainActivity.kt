082011ea9c7e367a1749b8563c89865807c837e1
orthh <witchicken2@gmail.com>
2023-08-28 12:24:55 +0900

    dir: 디렉토리 추가

--- client/healthapp/app/src/main/java/com/blueme/healthapp/MainActivity.kt
+++ client/healthapp/app/src/main/java/com/blueme/healthapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.blueme.healthapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
     }
 }
---
+++ server/src/main/java/com/blueme/backend/web/IndexController.java
@@ -0,0 +1,15 @@
+package com.blueme.backend.web;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+	
+	
+	
+}
