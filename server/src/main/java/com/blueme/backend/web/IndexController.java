549f2c5332148754c6071d84863232696719c400
orthh <witchicken2@gmail.com>
2023-08-28 12:49:24 +0900

    feat : DB연동

--- server/src/main/java/com/blueme/backend/web/IndexController.java
+++ server/src/main/java/com/blueme/backend/web/IndexController.java
@@ -2,7 +2,11 @@ package com.blueme.backend.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +17,11 @@ import lombok.extern.slf4j.Slf4j;
 @CrossOrigin("http://localhost:3000")
 public class IndexController {
 	
+	// 테스트용
+	@GetMapping("/home/reservation/add")
+	public Long reservation() {
+		return 1L;
+	}
 	
 	
 }
