6ada98aa1625dbdc59d32bb2bed9683c9c8b617f
이지희 <jihilli@naver.com>
2023-08-29 10:38:41 +0900

    jihee error fixed

--- client/react/src/App.js
+++ client/react/src/App.js
@@ -1,40 +1,31 @@
-<<<<<<< HEAD
-import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/footer";
-import Main from "./components/Main";
-import Login from "./components/Login";
-import Join from "./components/Join";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-=======
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/footer';
 import Main from './components/Main';
-import Login from './components/Login'; // TestTest 컴포넌트의 이름은 대문자로 시작해야 합니다.
+import Login from './components/Login';
+import Join from './components/Join';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
->>>>>>> origin/jihee
 
 const App = () => {
-  return (
-    <Router>
-      <div>
-        <Header />
+    return (
+        <Router>
+            <div>
+                <Header />
 
-        {/* Routes와 Route를 사용하여 경로에 따라 다른 페이지 렌더링 */}
-        <Routes>
-          {/* path 속성은 주어진 경로가 정확히 일치할 때만 해당 Route가 작동하도록 합니다 */}
-          {/* 예: /testtest 경로는 Main과 일치하지 않으므로 Main은 렌더링되지 않습니다 */}
-          <Route path="/" element={<Main />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Join" element={<Join />} />
-          {/* 필요한 경우 추가 Route 설정 */}
-        </Routes>
+                {/* Routes와 Route를 사용하여 경로에 따라 다른 페이지 렌더링 */}
+                <Routes>
+                    {/* path 속성은 주어진 경로가 정확히 일치할 때만 해당 Route가 작동하도록 합니다 */}
+                    {/* 예: /testtest 경로는 Main과 일치하지 않으므로 Main은 렌더링되지 않습니다 */}
+                    <Route path="/" element={<Main />} />
+                    <Route path="/Login" element={<Login />} />
+                    <Route path="/Join" element={<Join />} />
+                    {/* 필요한 경우 추가 Route 설정 */}
+                </Routes>
 
-        <Footer />
-      </div>
-    </Router>
-  );
+                <Footer />
+            </div>
+        </Router>
+    );
 };
 
 export default App;
