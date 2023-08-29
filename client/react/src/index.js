3b5e97935a569035b46858e952eb057271c6c364
orthh <witchicken2@gmail.com>
2023-08-29 18:20:10 +0900

    googletest

--- client/react/src/index.js
+++ client/react/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <App />
+  <GoogleOAuthProvider clientId="918319240725-4nvnocn81iriqjc64nigemgebntrkk25">
+    <App />
+  </GoogleOAuthProvider>
   // </React.StrictMode>
 );
 
