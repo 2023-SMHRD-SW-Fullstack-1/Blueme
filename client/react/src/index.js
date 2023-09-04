1bbb4bc170e63b6194270a43fec9ff1b96bbd7e8
이지희 <jihilli@naver.com>
2023-09-04 09:50:46 +0900

    feat : 라이브러리 추가

--- client/react/src/index.js
+++ client/react/src/index.js
@@ -1,17 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <GoogleOAuthProvider clientId="918319240725-4nvnocn81iriqjc64nigemgebntrkk25">
+    // <React.StrictMode>
     <App />
-  </GoogleOAuthProvider>
-  // </React.StrictMode>
+    // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
