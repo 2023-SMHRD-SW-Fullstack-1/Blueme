e79f44ed93ce02f04461883d3d7d4785dcdb54f3
Jihoon0127 <jihoong0127@gmail.com>
2023-09-08 17:34:01 +0900

    fixed : three.js

--- client/react/src/pages/rec/LoadGpt.jsx
+++ client/react/src/pages/rec/LoadGpt.jsx
@@ -1,21 +1,45 @@
-import React, { useEffect, useState } from "react";
-import loading from "../../assets/img/loading.png";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-
-import { Canvas } from "@react-three/fiber";
+import React from "react";
+import { Canvas, useThree, useFrame } from "@react-three/fiber";
 import { Model } from "../Model";
 import { OrbitControls } from "@react-three/drei";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
+
+const DirectionalLightWithCamera = () => {
+  const { camera } = useThree();
+  const lightRef = React.useRef();
+
+  useFrame(() => {
+    if (lightRef.current) {
+      lightRef.current.position.copy(camera.position);
+    }
+  });
+
+  return (
+    <directionalLight
+      ref={lightRef}
+      castShadow
+      intensity={50}
+      shadow-mapSize-width={1024}
+      shadow-mapSize-height={1024}
+      shadow-camera-far={50}
+      shadow-camera-left={-10}
+      shadow-camera-right={10}
+      shadow-camera-top={10}
+      shadow-camera-bottom={-10}
+    />
+  );
+};
 
 const LoadGpt = () => {
   const navigate = useNavigate();
 
-  //3초 후 RecPlayList로 이동
   const timeout = () => {
     setTimeout(() => {
       navigate("/RecPlayList");
-    }, 5000);
+    }, 3000);
   };
+
   useEffect(() => {
     timeout();
     return () => {
@@ -24,14 +48,26 @@ const LoadGpt = () => {
   });
 
   return (
-    // GPT 추천 로딩 화면
-    <div className="bg-gradient-to-t from-gray-900 via-stone-950 to-gray-700 text-custom-white flex flex-col min-h-screen justify-center items-center text-2xl font-semibold tracking-tighter">
+    <div className="bg-gradient-to-t from-gray-900 via-stone-950 to-gray-700 text-custom-white flex flex-col h-full justify-center items-center text-2xl font-semibold tracking-tighter space-y-10">
       <p className="mb-3">GPT가 추천해주고 있어요</p>
-      <img
-        src={loading}
-        alt="내 데이터 로딩 중"
-        class="object-cover w-[180px] h-[20px] mb-28"
-      />
+
+      <div className="from-gray-900 via-stone-950 to-gray-700 w-[100%]">
+        <Canvas
+          camera={{
+            fov: 30,
+            near: 1,
+            aspect: window.innerWidth / window.innerHeight,
+            far: 100,
+            position: [0, 0, 1.4],
+          }}
+        >
+          <pointLight color={"white"} intensity={50} />
+          <ambientLight intensity={50} />
+          <DirectionalLightWithCamera />
+          <Model />
+          <OrbitControls minDistance={1} maxDistance={10} />
+        </Canvas>
+      </div>
     </div>
   );
 };
