b12baab88396c1ac0b861feb2aaf25e74f009171
이지희 <jihilli@naver.com>
2023-09-12 09:20:47 +0900

    라이브러리 수정

--- client/react/src/components/Library/LikedList.jsx
+++ client/react/src/components/Library/LikedList.jsx
@@ -1,12 +1,11 @@
 /*
 작성자: 이지희
 날짜(수정포함): 2023-09-11
-설명: 라이브러리 페이지 내 좋아요 누른 곡 리스트 
+설명: 라이브러리 페이지 내 좋아요 누른 곡 5개 리스트 
 */
 
 import { useEffect, useState } from "react";
 import SingleMusic from './SingleMusic.jsx';
-import dummy from '../../dummy/MusicDummy.json';
 import axios from 'axios';
 
 
@@ -18,25 +17,25 @@ const [likedMusics, setLikedMusics] = useState([]);
 
 useEffect(() => {
   const fetchLikedList = async () => {
-    try {
+    try { 
       const response = await axios.get(`/likemusics/${userId}`);
-      let musicArray = response.data.map((item) => ({
-        id: item.musicId,
-        title: item.title,
-        artist: item.artist,
-        img: item.img,
-      }));
-      setLikedMusics(musicArray);
+      setLikedMusics(response.data);
+      console.log('likedMusics',likedMusics);
     } catch (error) {
-      console.error("좋아요 목록 불러오기 실패", error);
+      console.error(`Error: ${error}`);
     }
   };
+
   fetchLikedList();
-}, [userId]);
+}, []);
+
+// console.log('musics', likedMusics);
+// console.log('id', likedMusics[0].id);
+
     return (
         <div>
-            {likedMusics.slice(0, 5).map((item) => (
-                <SingleMusic key={item.id} item={item} />
+            {likedMusics.slice(0, 5).map((song) => (
+                <SingleMusic key={song.id} item={song} />
             ))}
         </div>
     );
