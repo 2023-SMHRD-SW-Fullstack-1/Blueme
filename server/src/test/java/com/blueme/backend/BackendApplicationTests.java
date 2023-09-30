74c0983dacb8cfe8c3be03a227dca4a5f23e0868
orthh <witchicken2@gmail.com>
2023-09-30 10:35:11 +0900

    fix(boot) - build중 contextLoad() 에러 해결- junit 의존성주입

--- server/src/test/java/com/blueme/backend/BackendApplicationTests.java
+++ server/src/test/java/com/blueme/backend/BackendApplicationTests.java
@@ -2,8 +2,17 @@ package com.blueme.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+/**
+ * jUnit 테스트 프로파일 생성 추가
+ * 
+ * @author김혁
+ * @version 1.0
+ * @since 2023-09-30
+ */
 @SpringBootTest
+@ActiveProfiles("jwt")
 class BackendApplicationTests {
 
 	@Test
